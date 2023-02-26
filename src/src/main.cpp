f90957216736be34a27594278b84398bf9ce0465
vitalyos <vitalyos@users.noreply.github.com>
2023-02-26 10:10:56 +0000

    Added google test

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,9 +1,25 @@
 #include <glog/logging.h>
 #include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+class Evaluate : public testing::Test
+{
+    public:
+};
+
+TEST_F(Evaluate, SimpleTsest)
+{
+    ASSERT_EQ(2, 1 + 1);
+}
 
 int main(int argc, char ** argv)
 {
     google::InitGoogleLogging(argv[0]);
     google::ParseCommandLineFlags(&argc, &argv, true);
+    testing::InitGoogleTest(&argc, argv);
+
     LOG(INFO) << "SUCCESS";
+    
+    return RUN_ALL_TESTS();
+
 }
